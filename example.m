bdd224d0ba5ee80986108e66edeb68d6e0e9f649
alavendelm <alavendelm@gmail.com>
2017-08-23 10:01:05 -0700

    edit example script

--- example.m
+++ example.m
@@ -3,6 +3,8 @@ clc;
 addpath('Data');
 addpath('NNDSVD');
 addpath('symnmf2');
+
+% Select and load data as matrix: genes x cells 
 % data = importdata('data.txt');
 load HEE_matrix.mat;
 data = HEE_matrix';
@@ -12,7 +14,7 @@ alpha = 0.5;
 beta = 0.5;
 data = processdata(data,alpha,beta);
 
-NC = [];
 %% Run SOptSC
+NC = [];
 init_point = 1;
 [W,P,No_cluster,cluster_label,cell_order] = SOptSC(data,init_point,NC);
