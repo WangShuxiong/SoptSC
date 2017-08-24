cab4a74ab4ed0d2f48dbfd08945b3d5549fbd63a
alavendelm <alavendelm@gmail.com>
2017-08-24 14:51:36 -0700

    update example and readme

--- example.m
+++ example.m
@@ -1,17 +1,19 @@
+%% Setup paths and load data
 clear;
 clc;
+echo on;
 addpath('Data');
 addpath('NNDSVD');
 addpath('symnmf2');
 
-% Select and load data as matrix: genes x cells 
-% data = importdata('data.txt');
+% Select and load data matrix in format: Genes x Cells 
 load HEE_matrix.mat;
 data = HEE_matrix';
 
 %% Optional step: preprocess data by selecting a subset of genes
-alpha = 0.5;
-beta = 0.5;
+alpha = 0.5; % 
+beta = 0.5;  % Number of cells in which a gene is expressed
+             % (threshold ratio)
 data = processdata(data,alpha,beta);
 
 %% Run SOptSC
