b66e9533ea96c2206d7d8fbaa68bdf5952ab07ee
WangShuxiong <wsx@lsec.cc.ac.cn>
2017-12-21 15:53:14 -0800

    update

--- example.m
+++ example.m
@@ -12,7 +12,7 @@ data = HEE_matrix';
 
 % Load all gene names as string
 load allgenes.mat;
-
+% allgenes = allgenes;
 % load Test_3_Pollen.mat;
 % data = in_X';
 %% Optional step: preprocess data by selecting a subset of genes
@@ -20,7 +20,7 @@ alpha = 0.5; % Variance in gene expression (threshold)
 beta = 0.5;  % Number of cells in which a gene is expressed
              % (threshold ratio)
 
-data_processed = processdata(data,alpha,beta);
+[select_genes,data_processed] = processdata(data,alpha,beta);
 
 
 %% Step 1: Run SoptSC to identify clusters and subpopulation composition
@@ -46,9 +46,9 @@ plot_genes(gene_set,allgenes,data,latent)
 %% Step 3: Plot gene-cell heatmap
 
 topn = 20;
-Gene_labels = GC_heatmapTopn(data_processed,cluster_label,H,allgenes,gene_idx,topn);
+Gene_labels = GC_heatmapTopn(data_processed,cluster_label,H,allgenes,select_genes,topn);
 %% Step 4: Run SoptSC to infer pseudotime and lineage tree
-init_cluster = 1;               % Starting cluster specified by user
+init_cluster = 2;               % Starting cluster specified by user
                                 % based on the analysis from step 2
 cell_order = SOptSC_pseudotime(init_cluster,P,cluster_label,latent,No_cluster);
 
