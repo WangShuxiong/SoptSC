85484e43a7ff700896fac450cf48f22280dfea02
WangShuxiong <wsx@lsec.cc.ac.cn>
2017-12-21 16:22:25 -0800

    Num_cluster

--- Num_cluster.m
+++ Num_cluster.m
@@ -6,32 +6,17 @@ nno_cluster = 1:25; tau = 0.3; tol = 0.01;
 
 zz = sort(abs(real(all_eigs)));
 
-% zz_idx = find(zz<=1);
-% ZZ = zz(zz_idx);
 ZZ = zz;
 
 if length(ZZ)>=2
     gap = ZZ(2:end) - ZZ(1:end-1);
     [~,No_cluster1] = max(gap);
-%     display(gapval);
-%     display(No_cluster1);
 end
 
-No_cluster2 = length(find(zz<=tol));
+% No_cluster2 = length(find(zz<=tol));
 No_cluster = No_cluster1;
-display('Number of clusters based on number of zero eigenvalues & largest eigengap ');
-display([No_cluster2 No_cluster]);
+display('Number of clusters inferred by SoptSC is:');
+display(No_cluster);
 
-% display('First 50 eigenvalues of graph Laplacian:');
-% display(zz(1:50));
-% 
-% figure;
-% scatter(1:30,zz(1:30),20,'filled');
-% box on;
-% set(gca,'LineWidth',1.5);
-% xlabel('i');
-% ylabel('Eigenvalue of graph Laplacian \lambda_i');
-% set(gca,'FontName','Arial');
-% set(gca,'FontSize',12);
 eigenvalues = zz;
 end
