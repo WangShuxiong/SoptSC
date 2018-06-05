3c14e7e33b9b405f5ce6735a318e12edd0d78c75
Adam L MacLean <alavendelm@gmail.com>
2018-06-05 15:06:37 -0700

    update plots locations

--- plot_sig_network.m
+++ plot_sig_network.m
@@ -1,4 +1,4 @@
-function plot_sig_network(Pidv,Pall,cluster_label,Lig,Rec,threshold,folder)
+function plot_sig_network(Pidv,Pall,cluster_label,Lig,Rec,threshold,resfolder)
 % plot cell-cell and cluster-cluster signaling network based on the probability matrix
 % Input:
 %   -- Pidv: Cell-to-cell interaction probability of individual
@@ -108,7 +108,7 @@ for j = 1:No_LR
     colormap(mycolor);
     colorbar('Ticks',tickval,'TickLabels',lgd,'FontSize',12);
     
-    print([folder '\Cell_LR_' a{1} '_' b{1}],'-dpdf','-r300');
+    print([resfolder '\Cell_LR_' a{1} '_' b{1}],'-dpdf','-r300');
 end
 
 
@@ -133,7 +133,7 @@ title('cell\_LR\_all\_pairs','fontsize',12);
 colormap(mycolor);
 colorbar('Ticks',tickval,'TickLabels',lgd,'FontSize',12);
 
-print([folder '\Cell_LR_all_pairs'],'-dpdf','-r300');
+print([resfolder '\Cell_LR_all_pairs'],'-dpdf','-r300');
 
 
 
@@ -168,7 +168,7 @@ for j = 1:No_LR
     colormap(mycolor);
     colorbar('Ticks',tickval,'TickLabels',lgd,'FontSize',12);
     
-    print([folder '\Cluster_LR_' a{1} '_' b{1}],'-dpdf','-r300');
+    print([resfolder '\Cluster_LR_' a{1} '_' b{1}],'-dpdf','-r300');
 end
 
 
@@ -201,7 +201,7 @@ end
     colormap(mycolor);
     colorbar('Ticks',tickval,'TickLabels',lgd,'FontSize',12);
     
-    print([folder '\Cluster_LR_all_pairs'],'-dpdf','-r300');
+    print([resfolder '\Cluster_LR_all_pairs'],'-dpdf','-r300');
 
 
 
